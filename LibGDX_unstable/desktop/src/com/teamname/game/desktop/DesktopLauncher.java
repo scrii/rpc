a73989ec97fce6dae01de27c15d8dcc3f5dc7ffd
scrii <79646124+scrii@users.noreply.github.com>
2021-02-26 21:57:45 +0500

    Version of the game part being developed

---
+++ LibGDX_unstable/desktop/src/com/teamname/game/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package com.teamname.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.teamname.game.Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width=1024;
+		config.height=720;
+		new LwjglApplication(new Main(), config);
+	}
+}
